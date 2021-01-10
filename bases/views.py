af6523c069e9aee9ba914a59dc75e8064a696a78
conejofoz <conejofoz@gmail.com>
2021-01-10 18:15:29 -0300

    template configurado e funcional

---
+++ bases/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
+
+
+class Home(LoginRequiredMixin, generic.TemplateView):
+    template_name = 'bases/home.html'
+    login_url = '/admin'
