493885934856e1dbc3e93d3a62e05de637f579df
conejofoz <conejofoz@gmail.com>
2021-01-16 08:05:50 -0300

    categoria new

---
+++ inv/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+from .models import Categoria
+
+class CategoriaForm(forms.ModelForm):
+    class Meta:
+        model=Categoria
+        fields=['descricao', 'estado']
+        labels={'descricao':'Descrição da categoria', 'estado':'Estado'}
+        widget={'descricao':forms.TextInput}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({'class':'form-control'})
